d9e2d4ab467c11746d3895cc4911f08f978a2279
Rieyu <21bd1a0505csea@gmail.com>
2024-05-29 15:59:10 +0530

    Added POST route and Form component

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth"
 import { eq } from "drizzle-orm";
+import {zValidator} from "@hono/zod-validator"
+import {createId} from "@paralleldrive/cuid2"
 
 const app = new Hono()
     .get('/',
@@ -10,7 +12,7 @@ const app = new Hono()
         async (c) => {
             const auth = getAuth(c);
             if (!auth?.userId) {
-                return c.json({error:"Unauthorized"},401);
+                return c.json({ error: "Unauthorized" }, 401);
                 // throw new HTTPException(
                 //     401,
                 //     {
@@ -30,6 +32,31 @@ const app = new Hono()
                     eq(accounts.userId, auth.userId),
                 );
             return c.json({ data });
-        });
+        }
+    )
+    .post(
+        "/",
+        clerkMiddleware(),
+        zValidator("json",insertAccountSchema.pick({name: true})),
+        async (c)=>{
+            const auth = getAuth(c);
+            const values = c.req.valid("json");
+
+            if(!auth?.userId){
+                return c.json({error: "Unauthorized"}, 401);
+            }
+
+            const [data] = await db.insert(accounts).values(
+                {   
+                    id:createId(),
+                    userId: auth.userId,
+                    ...values
+
+                }
+            ).returning();
+
+            return c.json({data})
+        }
+    )
 
 export default app;
